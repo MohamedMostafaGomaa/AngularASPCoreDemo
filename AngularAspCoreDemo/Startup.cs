32e85766f0fcaf8b1183da329ce0abe3162a80a0
Mohamed Mostafa Gomaa <Moh.MostafaGomaa@gmail.com>
2018-12-05 14:30:51 -0500

    comment

--- AngularAspCoreDemo/Startup.cs
+++ AngularAspCoreDemo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AngularAspCoreDemo
 {
+    // start up
     public class Startup
     {
         public Startup(IConfiguration configuration)
